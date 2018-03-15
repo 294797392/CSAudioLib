9e07c67a590860de16ca72288a1d369020a4780c
294797392@qq.com <294797392@qq.com>
2018-03-16 02:05:17 +0800

    .

---
+++ DirectSoundLibTest/ResponseCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DirectSoundLibTest
+{
+    public class ResponseCode
+    {
+        public static readonly int Success = 0;
+    }
+}
